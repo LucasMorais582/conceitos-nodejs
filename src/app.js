f9511516097146ba26abc5924390e133daba83cb
Lucas <lucasmorais582@gmail.com>
2020-04-20 20:44:40 -0300

    Finalização da aplicação

---
+++ src/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id))
+    return response.status(400).json({ error: "Id invalid."});
+
+  return next();
+}
+
+app.get("/repositories", (request, response) => {
+  response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs, likes } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes };
+  
+  // Setando o número inicial de likes
+  repository.likes = 0;
+  
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: "Repository not found."});
+
+  const repository = { id, title, url, techs, likes };
+  
+  //Mantendo o número de likes
+  repository.likes = repositories[repositoryIndex].likes;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: "Repository not found."});
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: "Repository not found."});
+
+  repositories[repositoryIndex].likes += 1;
+  return response.json(repositories[repositoryIndex]);  
+});
+
+module.exports = app;
